88a17d5c006eb843db37e639b8549f748bc55d3f
AltUser131183 <novachrono314@gmail.com>
2022-03-27 20:30:40 +0200

    automatic push from jetson nano

--- pushChanges.py
+++ pushChanges.py
@@ -1,7 +1,11 @@
 from git import Repo
 
-repo = Repo(".")
-repo.git.add(update=True)
-repo.index.commit("automatic push from jetson nano")
-origin = repo.remote(name="origin")
-origin.push()
+def pushChanges():
+	repo = Repo(".")
+	repo.git.add(update=True)
+	repo.index.commit("automatic push from jetson nano")
+	origin = repo.remote(name="origin")
+	origin.push()
+
+if __name__ == "__main__":
+	pushChanges()
