7acd79a76836b8384d29acea851239ef15c43bc4
marc131183 <marc.groeling@gmail.com>
2022-06-05 12:37:13 +0200

    added code for collecting images

---
+++ src/collectImages.py
@@ -0,0 +1,40 @@
+import os
+import time
+import numpy as np
+from PIL import Image, ImageDraw, ImageFont
+import RPi.GPIO as GPIO
+
+from IO import Camera, ImageViewer
+
+if __name__ == "__main__":
+    FOLDER = ""  # "/home/saadjahangir/Code/WasteClassification/"
+    COUNTDOWN_VALUE = 5  # number of seconds between shots
+    width, height = 100, 100
+
+    class_name = input("Enter class name: ")
+    save_dir = FOLDER + "data/unlabelled/" + class_name
+    if not os.path.isdir(save_dir):
+        os.mkdir(save_dir)
+        index = 0
+    else:
+        index = int(max([elem for elem in os.listdir(save_dir)])[:-4])
+
+    try:
+        cam = Camera()
+        imgV = ImageViewer()
+
+        remaining = COUNTDOWN_VALUE
+        font = ImageFont.truetype("DejaVuSans.ttf", 50)
+        while True:
+            img = Image.new("RGB", (width, height), (255, 255, 255))
+            draw = ImageDraw.Draw(img)
+            draw.text((0, 0), str(remaining), font=font, fill=(0, 0, 0))
+            imgV.setImage(img)
+            remaining -= 1
+            if not remaining:
+                remaining = COUNTDOWN_VALUE
+                index += 1
+                cam.savePicture(save_dir + "/{}.png".format(index))
+            time.sleep(1)
+    finally:
+        GPIO.cleanup()
