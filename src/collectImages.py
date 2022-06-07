c8b18c4eddbcad7e5cf23e8a461d524cc3c42232
marc131183 <marc.groeling@gmail.com>
2022-06-07 15:24:27 +0200

    adjusted image size for collecting images

--- src/collectImages.py
+++ src/collectImages.py
@@ -2,6 +2,7 @@ import os
 import time
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
+
 import RPi.GPIO as GPIO
 
 from IO import Camera, ImageViewer
@@ -9,7 +10,6 @@ from IO import Camera, ImageViewer
 if __name__ == "__main__":
     FOLDER = ""  # "/home/saadjahangir/Code/WasteClassification/"
     COUNTDOWN_VALUE = 5  # number of seconds between shots
-    width, height = 100, 100
 
     class_name = input("Enter class name: ")
     save_dir = FOLDER + "data/unlabelled/" + class_name
@@ -22,6 +22,7 @@ if __name__ == "__main__":
     try:
         cam = Camera()
         imgV = ImageViewer()
+        width, height = imgV.screen_width, imgV.screen_height
 
         remaining = COUNTDOWN_VALUE
         font = ImageFont.truetype("DejaVuSans.ttf", 50)
@@ -29,6 +30,7 @@ if __name__ == "__main__":
             img = Image.new("RGB", (width, height), (255, 255, 255))
             draw = ImageDraw.Draw(img)
             draw.text((0, 0), str(remaining), font=font, fill=(0, 0, 0))
+            img.show()
             imgV.setImage(img)
             remaining -= 1
             if not remaining:
