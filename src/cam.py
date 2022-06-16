5dde8c070b3074b314070f70c6515c5bdcddacd5
61HzPentile <h804347394@gmail.com>
2022-06-16 13:22:21 +0200

    test

--- src/cam.py
+++ src/cam.py
@@ -9,7 +9,7 @@ cam = cv2.VideoCapture(0)
 for i in range(100):
     worked, frame = cam.read()
 
-    # cv2.imshow('img', frame)
+    # cv2.imshow('img', frame) m
 
     cv2.imwrite("data/image{}.png".format(i), frame)
     time.sleep(1)
