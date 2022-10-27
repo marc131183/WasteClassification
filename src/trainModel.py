d6537b2e28ed421fb719fce66f55025494c2d9c3
marc131183 <marc.groeling@gmail.com>
2022-10-27 13:48:42 +0200

    fix errors in trainModel script

--- src/trainModel.py
+++ src/trainModel.py
@@ -23,18 +23,19 @@ if __name__ == "__main__":
         save_dir = os.getcwd() + "/WasteClassification/data/models/resnet50.pt"
         init_function = resnet50
 
-    data_dir = os.getcwd() + "/WasteClassification/data/all"
+    data_dir = os.getcwd() + "/WasteClassification/data/classification/all"
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     image_datasets = datasets.ImageFolder(data_dir, DATA_TRANSFORMS["train"])
     dataset_sizes = {x: len(image_datasets) for x in ["train"]}
     data_loaders = {
         x: torch.utils.data.DataLoader(
-            image_datasets[x], batch_size=BATCH_SIZE, shuffle=True, num_workers=10
+            image_datasets, batch_size=BATCH_SIZE, shuffle=True, num_workers=10
         )
         for x in ["train"]
     }
 
+    print("-" * 15, "Start training {} model".format(model_type), "-" * 15)
     model = train_model_optional_validation(
         *init_function(), data_loaders, dataset_sizes, device, max_epochs=NUM_EPOCHS
     )
